fe4e6aa741c1879aea7157b1e865509086403dd9
egogoboy <egor2005sudakov@gmail.com>
2026-02-18 21:12:07 +0300

    nvim: change visual mode color

--- .config/nvim/lua/config/appearance/nordic.lua
+++ .config/nvim/lua/config/appearance/nordic.lua
@@ -2,6 +2,9 @@ require('nordic').setup({
     transparent = {
         bg = true,
     },
+    visual = {
+        theme = 'light',
+    },
     on_palette = function(palette)
         palette.black0 = "#1E1E1E"
     end,
